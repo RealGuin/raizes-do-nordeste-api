b9a3b482dfc866e49b7abef9699844c5c47ac650
RealGuin <guinppereira@gmail.com>
2026-03-07 11:29:32 -0300

    Inicializa projeto Spring boot

---
+++ src/main/java/com/raizesdonordeste/raizesnovoapi/RaizesnovoApiApplication.java
@@ -0,0 +1,13 @@
+package com.raizesdonordeste.raizesnovoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaizesnovoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaizesnovoApiApplication.class, args);
+	}
+
+}
